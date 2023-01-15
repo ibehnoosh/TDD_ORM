280db87b552a42605e1c1a90bbdb264ade48932b
Behnoosh Moshtagh <behnoosh.moshtagh@gmail.com>
2023-01-15 23:42:18 +0330

    Config Test

--- tests/Unit/ConfigTest.php
+++ tests/Unit/ConfigTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Tests\Unit;
 
+use App\Exceptions\ConfigFileNotFoundException;
 use App\Helpers\Config;
 use PHPUnit\Framework\TestCase;
 
@@ -10,5 +11,25 @@ class ConfigTest extends TestCase
     {
         $config=Config::getFileContents('database');
         $this->assertIsArray($config);
+    }
+
+    public function  testIfFileNotExistThrowException()
+    {
+        $this->expectException(ConfigFileNotFoundException::class);
+        $config=Config::getFileContents('dummy');
+    }
+
+    public function testGetMethodReturnValid()
+    {
+        $config=Config::get('database','pdo');
+        $exceptedData =[
+            'driver' => 'mysql',
+            'host'=> '127.0.0.1',
+            'database'=>'bug_tracker',
+            'db_user'=>'root',
+            'db_password' => ''
+        ];
+        $this->assertEquals($config,$exceptedData);
+
     }
 }
