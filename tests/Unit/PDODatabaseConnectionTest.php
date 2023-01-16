c5ffa97579a4e5eee5e62150bfa432cd707b0e21
Behnoosh Moshtagh <behnoosh.moshtagh@gmail.com>
2023-01-16 12:22:03 +0330

    Connection Test

--- tests/Unit/PDODatabaseConnectionTest.php
+++ tests/Unit/PDODatabaseConnectionTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit;
 
 use App\Contracts\DatabaseConnectionInterface;
 use App\Database\PDODatabaseConnection;
+use App\Exceptions\DatabaseConnectionException;
 use App\Helpers\Config;
 use PDO;
 use PHPUnit\Framework\TestCase;
@@ -16,12 +17,32 @@ class PDODatabaseConnectionTest extends TestCase
         $pdoConnection= new PDODatabaseConnection($config);
         $this->assertInstanceOf(DatabaseConnectionInterface::class, $pdoConnection);
     }
-    public function testConnectFuncShouldBeConnectToDatabase()
+
+    public function testConnectFunctionShouldReturnValidInstance()
     {
         $config= $this->getConfig();
         $pdoConnection= new PDODatabaseConnection($config);
         $pdoConnection->connect();
+        $this->assertInstanceOf(PDODatabaseConnection::class,$pdoConnection);
+        return $pdoConnection;
+    }
+
+    /**
+     * @return void
+     * @throws DatabaseConnectionException
+     * @depends testConnectFunctionShouldReturnValidInstance
+     */
+    public function testConnectFuncShouldBeConnectToDatabase($pdoConnection)
+    {
         $this->assertInstanceOf(PDO::class,$pdoConnection->getConnection());
+    }
+    public function testItThrowExceptionIfConfigIsInvalid()
+    {
+        $this->expectException(DatabaseConnectionException::class);
+        $config= $this->getConfig();
+        $config['database']='dummy';
+        $pdoConnection= new PDODatabaseConnection($config);
+        $pdoConnection->connect();
     }
     private function getConfig()
     {
